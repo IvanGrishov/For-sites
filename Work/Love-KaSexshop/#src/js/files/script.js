442915ac6e96834995d8946cacae3396de80d463
IvanGrishov <ivangrishov@gmail.com>
2020-12-11 16:05:35 +0300

    apdate

--- Work/Love-KaSexshop/#src/js/files/script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('click', function (e) {
-    if (e.target.closest(".menu__link") || e.target.closest(".advantages__bottom")) {
-        let user_menu = document.querySelector('.menu__body');
-        let body = document.querySelector('body');
-        user_menu.classList.remove("_active");
-        body.classList.remove("_lock");
-    }
-});
